a317b281186476c3f239b6a5f5223b968586264c
Yulian Belov <belovyulian@gmail.com>
2023-07-06 13:05:18 +0300

    commit

--- nim.js
+++ nim.js
@@ -1,27 +1,30 @@
-// Get the required elements from the DOM
-const cubesContainer = document.getElementById('cubes-container');
+const marblesContainer = document.getElementById('marbles-container');
 const removeBtn = document.getElementById('remove-btn');
 const popupContainer = document.getElementById('popup-background');
-const startPopup = document.getElementById('start-popup');
+const countElement = document.getElementById('count');
 
-// Function to return the cubes and show the start popup
-function returnCubes() {
+function updateCounter() {
+  countElement.textContent = marblesRemaining;
+}
+
+function returnMarbles() {
   // Clear the cubes container
-  cubesContainer.innerHTML = '';
+  marblesContainer.innerHTML = '';
 
   // Create and append the cubes back to the container
   for (let i = 0; i < 21; i++) {
-    const cube = document.createElement('div');
-    cube.classList.add('cube');
-    cubesContainer.appendChild(cube);
+    const marble = document.createElement('div');
+    marble.classList.add('marble');
+    marblesContainer.appendChild(marble);
   }
 
   // Reattach event listeners to the cubes for selection
-  const cubes = cubesContainer.getElementsByClassName('cube');
-  for (let i = 0; i < cubes.length; i++) {
-    cubes[i].addEventListener('click', handleCubeSelection);
+  const marbles = marblesContainer.getElementsByClassName('marble');
+  for (let i = 0; i < marbles.length; i++) {
+    marbles[i].addEventListener('click', handleMarbleSelection);
   }
-  cubesRemaining = 21;
+  marblesRemaining = 21;
+  updateCounter();
 
   setTimeout(() => {
     popupContainer.style.display = 'block';
@@ -29,13 +32,15 @@ function returnCubes() {
 }
 
 // Number of cubes remaining
-let cubesRemaining = 21;
+let marblesRemaining = 21;
+
+updateCounter();
 
 // Enable the remove button
 removeBtn.disabled = true;
 
 // Track the selected cubes by the player
-let selectedCubes = [];
+let selectedMarbles = [];
 
 // Track the current player's turn
 let currentPlayer;
@@ -43,16 +48,16 @@ let currentPlayer;
 popupContainer.style.display = 'block';
 
 // Add event listeners to the cubes for selection
-const cubes = cubesContainer.getElementsByClassName('cube');
-for (let i = 0; i < cubes.length; i++) {
-  cubes[i].addEventListener('click', handleCubeSelection);
+const marbles = marblesContainer.getElementsByClassName('marble');
+for (let i = 0; i < marbles.length; i++) {
+  marbles[i].addEventListener('click', handleMarbleSelection);
 }
 
 // Add event listener to the remove button
-removeBtn.addEventListener('click', handleRemoveCubes);
+removeBtn.addEventListener('click', handleRemoveMarbles);
 
 // Function to handle cube selection
-function handleCubeSelection() {
+function handleMarbleSelection() {
   // Check if it's the player's turn
   if (currentPlayer !== 'player') {
     return;
@@ -64,48 +69,48 @@ function handleCubeSelection() {
   // If already selected, remove the selection
   if (isSelected) {
     this.classList.remove('selected');
-    selectedCubes = selectedCubes.filter(cube => cube !== this);
+    selectedMarbles = selectedMarbles.filter(cube => cube !== this);
   } else {
     // If not selected, add the selection
-    if (selectedCubes.length < 3) {
+    if (selectedMarbles.length < 3) {
       this.classList.add('selected');
-      selectedCubes.push(this);
+      selectedMarbles.push(this);
     }
   }
 }
 
-function handleRemoveCubes() {
-  // Check if it's the player's turn
+function handleRemoveMarbles() {
   if (currentPlayer === 'player') {
     // Check if the player has selected at least 1 cube
-    if (selectedCubes.length === 0) {
-      alert('Please select at least 1 cube to remove.');
+    if (selectedMarbles.length === 0) {
+      alert('Please select at least 1 marble to remove.');
       return;
     }
 
     // Get the number of cubes to remove based on the player's selection
-    const cubesToRemove = selectedCubes.length;
+    const marbleToRemove = selectedMarbles.length;
 
     // Remove the cubes visually
-    for (let i = 0; i < cubesToRemove; i++) {
-      const cube = selectedCubes[i];
-      cubesContainer.removeChild(cube);
+    for (let i = 0; i < marbleToRemove; i++) {
+      const cube = selectedMarbles[i];
+      marblesContainer.removeChild(cube);
     }
 
     // Update the cubes remaining
-    cubesRemaining -= cubesToRemove;
+    marblesRemaining -= marbleToRemove;
+    updateCounter();
 
     // Clear the selected cubes array
-    selectedCubes = [];
+    selectedMarbles = [];
     currentPlayer = 'computer';
     // Disable the remove button to prevent further moves by the player
     removeBtn.disabled = true;
 
     // Check if the player wins after their move
-    if (cubesRemaining === 0) {
+    if (marblesRemaining === 0) {
       removeBtn.disabled = true;
-      alert('You won! You took the last cube.');
-      returnCubes();
+      alert('You won! You took the last marble.');
+      returnMarbles();
     }
   }
 
@@ -113,16 +118,18 @@ function handleRemoveCubes() {
     // Wait for 1 second before the computer makes its move
     setTimeout(() => {
       // Computer's turn - generate its move
-      const computerMove =  cubesRemaining === 20 ? Math.floor(Math.random() * 3 + 1) : cubesRemaining % 4;
+      const computerMove = marblesRemaining === 20 ? Math.floor(
+          Math.random() * 3 + 1) : marblesRemaining % 4;
 
       // Remove the cubes visually for the computer's move
       for (let i = 0; i < computerMove; i++) {
-        const cube = cubesContainer.lastElementChild;
-        cubesContainer.removeChild(cube);
+        const cube = marblesContainer.lastElementChild;
+        marblesContainer.removeChild(cube);
       }
 
       // Update the cubes remaining after the computer's move
-      cubesRemaining -= computerMove;
+      marblesRemaining -= computerMove;
+      updateCounter();
 
       // Enable the remove button for the player's turn
       removeBtn.disabled = false;
@@ -131,43 +138,41 @@ function handleRemoveCubes() {
       currentPlayer = 'player';
 
       // Check if the computer took the last cube
-      if (cubesRemaining <= 0) {
+      if (marblesRemaining <= 0) {
         removeBtn.disabled = true;
         setTimeout(() => {
-          alert('You lost! The computer took the last cube.');
-          returnCubes();
-        }, 500); // Delay the alert for 0.5 seconds
+          alert('You lost! The computer took the last marble.');
+          returnMarbles();
+        }, 500);
       }
-    }, 1000); // Delay for 1 second (1000 milliseconds)
+    }, 1000);
   }
 }
 
-// Function to start the game based on the selected starting player
 function startGame(startingPlayer) {
   currentPlayer = startingPlayer;
   popupContainer.style.display = 'none';
 
   if (currentPlayer === 'computer') {
-    // Wait for 1 second before the computer makes its first move
     setTimeout(() => {
-      // Computer's turn - generate its move
-      const computerMove = cubesRemaining % 4;
+      const computerMove = marblesRemaining % 4;
 
       // Remove the cubes visually for the computer's move
       for (let i = 0; i < computerMove; i++) {
-        const cube = cubesContainer.lastElementChild;
-        cubesContainer.removeChild(cube);
+        const cube = marblesContainer.lastElementChild;
+        marblesContainer.removeChild(cube);
       }
 
       // Update the cubes remaining after the computer's move
-      cubesRemaining -= computerMove;
+      marblesRemaining -= computerMove;
+      updateCounter();
 
       // Enable the remove button for the player's turn
       removeBtn.disabled = false;
 
       // Switch to the player's turn
       currentPlayer = 'player';
-    }, 1000); // Delay for 1 second (1000 milliseconds)
+    }, 1000);
   } else {
     // Enable the remove button for the player's turn
     removeBtn.disabled = false;
